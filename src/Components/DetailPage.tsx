115c4bd52812d3c60100471ae0fa237b1c0ab893
rp1405 <rpareek140503@gmail.com>
2024-03-14 12:38:27 +0530

    commit

--- src/Components/DetailPage.tsx
+++ src/Components/DetailPage.tsx
@@ -8,17 +8,17 @@ import { Tree } from "react-tree-graph";
 export default function DetailPage() {
   return (
     <>
-      <Header/>
-    <div className="w-[100vw] items-center justify-center">
-      <HeadingOfTopic
-        topicName="Frontend Developer"
-        topicDescription="Step by step guide to becoming a modern frontend developer in 2024"
-      />
-      <Roadmap />
-      <FAQ />
-    </div>
-      <CommunitySec/>
-      <Footer/>
-      </>
+      <Header />
+      <div className="w-[100vw] items-center justify-center">
+        <HeadingOfTopic
+          topicName="Frontend Developer"
+          topicDescription="Step by step guide to becoming a modern frontend developer in 2024"
+        />
+        <Roadmap />
+        <FAQ />
+      </div>
+      <CommunitySec />
+      <Footer />
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-            'node_modules/flowbite-react/lib/esm/**/*.js'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
-        'headerBgTop':'#0F172A',
-        'headerBgBottom':"#05080F",
-        'viewAllLeft':"#445265",
-        'viewAllRight':'#040406',
-        'guideSectionBg':"#F9FAFB"
-         // Replace with your desired color code
+        headerBgTop: "#0F172A",
+        headerBgBottom: "#05080F",
+        viewAllLeft: "#445265",
+        viewAllRight: "#040406",
+        guideSectionBg: "#F9FAFB",
+        // Replace with your desired color code
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("flowbite/plugin")],
 };
