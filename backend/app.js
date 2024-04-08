63085914070a43acfe08543e98c860d91dcf2100
rp1405 <rpareek140503@gmail.com>
2024-04-08 15:33:11 +0530

    minor

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const passport = require("passport");
 const app = express();
 
 const cors = require("cors");
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(
   cookieSession({
